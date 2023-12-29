39267c410b8ae89d2c63293c5c2a7d6f7dba91e6
etaelith <etaelithtest@gmail.com>
2023-12-29 14:59:03 -0300

    0.0.3b

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,21 @@
 import HomeClient from "./client";
 
 export default async function Home() {
-  const data = await fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales").then(
-    (res) => res.json() as Promise<{casa: {nombre: string; compra: string; venta: string}}[]>,
-  );
+  const res = await fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales", {
+    next: {revalidate: 60},
+  });
+  const data = (await res.json()) as Promise<
+    {casa: {nombre: string; compra: string; venta: string}}[]
+  >;
 
-  const cotizaciones = data
+  const cotizaciones = (await data)
     .filter((cotizacion) =>
       ["Dolar Oficial", "Dolar Bolsa", "Dolar Blue"].includes(cotizacion.casa.nombre),
     )
     .map((cotizacion) => ({
       nombre: cotizacion.casa.nombre,
-      compra: Number(cotizacion.casa.compra.split(",")[0]),
-      venta: Number(cotizacion.casa.venta.split(",")[0]),
+      compra: Number(cotizacion.casa.compra.replace(".", "").replace(",", ".")),
+      venta: Number(cotizacion.casa.venta.replace(".", "").replace(",", ".")),
     }));
 
   return <HomeClient cotizaciones={cotizaciones} />;
